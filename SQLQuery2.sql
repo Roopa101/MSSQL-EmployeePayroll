2e08ef8952df1d5a3bcb615bdf016df61ea1dc0d
Roopa Jamadar <roopajamadar14@gmail.com>
2021-12-30 19:17:11 +0530

    [Roopa] Added: Inserted Data into Table Employee_Payroll

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -9,5 +9,17 @@ create table employee_Payroll
  start_date date not null
 )
 
- select * from employee_Payroll
- 
+select * from employee_Payroll
+
+set identity_insert employee_payroll on
+
+insert into employee_payroll (id,name,salary,start_date) values
+(1,'billi', 230000.0,'2018-01-03')
+
+insert into employee_payroll (id,name,salary,start_date) values
+(2,'jack', 456000.0,'2019-11-13')
+
+insert into employee_payroll (id,name,salary,start_date) values
+(3,'charlie', 240000.0,'2019-01-03')
+
+set identity_insert employee_payroll off
