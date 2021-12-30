2007572a7554a7eba24d4fcef899cd99fc2d336d
Roopa Jamadar <roopajamadar14@gmail.com>
2021-12-30 19:05:25 +0530

    [Roopa] Added: Created Table Employee_Payroll

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,2 +1,13 @@
 create database EmployeePayroll_Service;
 use EmployeePayroll_Service;
+
+create table employee_Payroll
+(
+ id int IDENTITY(1,1) not null primary key,
+ name varchar(150) not null,
+ salary money not null,
+ start_date date not null
+)
+
+ select * from employee_Payroll
+ 
