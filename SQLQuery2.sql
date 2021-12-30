aebbf7789aebbb3fcbc235539883df211202cbbe
Roopa Jamadar <roopajamadar14@gmail.com>
2021-12-30 20:43:17 +0530

    [Roopa] Added: Extanded Employee_Payroll Table to have BasicPay,Deductions,TaxablePay,IncomeTax,NetPay

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -55,3 +55,18 @@ update employee_Payroll set Department = 'CS' where name = 'Billi';
 update employee_Payroll set Department = 'Mech' where name = 'jack';
 update employee_Payroll set Department = 'Production' where name = 'Charlie';
 select * from employee_Payroll
+
+
+
+Alter Table employee_Payroll Add Basic_Pay money;
+Alter Table employee_Payroll Add Deductions money;
+Alter Table employee_Payroll Add Taxable_Pay money;
+Alter Table employee_Payroll Add Income_Tax money;
+Alter Table employee_Payroll Add Net_Pay money;
+update employee_Payroll set Basic_Pay = '9000000.00' where name = 'Billi';
+update employee_Payroll set Basic_Pay = '90000.00' where name = 'jack';
+update employee_Payroll set Basic_Pay = '29000.00' where name = 'Charlie';
+update employee_Payroll set Deductions = '5638.00' where name = 'jack';
+update employee_Payroll set Taxable_Pay = '1234.00' where name = 'Charlie';
+update employee_Payroll set Income_Tax = '9153.00' where name = 'billi';
+update employee_Payroll set Net_Pay = '1539.00' where name = 'billi';
