aa65d84bb5788e5387e952f1fbd8c34e401a7001
Roopa Jamadar <roopajamadar14@gmail.com>
2021-12-31 15:49:26 +0530

    [Roopa] Added: Extended Employee_Payroll Table to have BasicPay,Deductions,TaxablePay,IncomeTax,NetPay

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -20,6 +20,9 @@ insert into employee_payroll (id,name,salary,start_date) values
 (2,'jack', 456000.0,'2019-11-13')
 
 insert into employee_payroll (id,name,salary,start_date) values
+(4,'Terissa', 560000.0,'2020-11-13')
+
+insert into employee_payroll (id,name,salary,start_date) values
 (3,'charlie', 240000.0,'2019-01-03')
 
 set identity_insert employee_payroll off
@@ -32,7 +35,7 @@ select * from employee_payroll where start_date between '2019-01-01' and GETDATE
 
 Alter table employee_payroll add gender char (1)
 update employee_payroll set gender = 'M' where name ='bill' or name= 'charlie'
-update employee_payroll set gender = 'F' where name= 'jack'
+update employee_payroll set gender = 'F' where name= 'jack' or name= 'Terissa'
 select * from employee_Payroll
 
 
@@ -51,9 +54,11 @@ select MAX(salary) 'Max Salary'from employee_Payroll where gender = 'F' group by
 alter table employee_Payroll Add Department varchar(10) ;
 alter table employee_Payroll Add Phonenumber varchar(15) ;
 Alter table employee_payroll add Address varchar(50) not null default 'India'
-update employee_Payroll set Department = 'CS' where name = 'Billi';
+update employee_Payroll set Department = 'Finance' where name = 'Billi';
 update employee_Payroll set Department = 'Mech' where name = 'jack';
 update employee_Payroll set Department = 'Production' where name = 'Charlie';
+update employee_Payroll set Department = 'Marketing' where name = 'Terissa';
+
 select * from employee_Payroll
 
 
@@ -66,7 +71,8 @@ Alter Table employee_Payroll Add Net_Pay money;
 update employee_Payroll set Basic_Pay = '9000000.00' where name = 'Billi';
 update employee_Payroll set Basic_Pay = '90000.00' where name = 'jack';
 update employee_Payroll set Basic_Pay = '29000.00' where name = 'Charlie';
-update employee_Payroll set Deductions = '5638.00' where name = 'jack';
+update employee_Payroll set Basic_Pay = '290000.00' where name = 'Terissa';
+update employee_Payroll set Deductions = '5638.00' where name = 'Terissa';
 update employee_Payroll set Taxable_Pay = '1234.00' where name = 'Charlie';
 update employee_Payroll set Income_Tax = '9153.00' where name = 'billi';
 update employee_Payroll set Net_Pay = '1539.00' where name = 'billi';
